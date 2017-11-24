326d02542fec053f4a74da93276c430cd54b6d7a
CamBallagh <ball0178@algonquinlive.com>
2017-11-24 09:57:29 -0500

    Namespacing fix
    Fixed issues

--- namespace.js
+++ namespace.js
@@ -1,28 +1,38 @@
 var BALL0178 = {
 
     init: function () {
+
+
         var box = document.createElement('div'),
-        boxes = document.getElementById('boxes');
+            boxes = document.getElementById('boxes');
+
+
         box.textContent = "BALL0178";
+
+
         box.classList.add('box');
+
         boxes.appendChild(box);
-        // EventListners Part
-        box.addEventListener('click', MKlick);
-        box.addEventListener('mouseover', MOver);
-        box.addEventListener('mouseout', MOut);
 
-        function MKlick(ev) {
-            ev.currentTarget.style.borderColor = "#0036FF";
-            ev.currentTarget.style.backgroundColor = "#FF0000";
 
-}
+        box.addEventListener('mouseover', mouseOver);
+        box.addEventListener('mouseout', mouseOut);
+        box.addEventListener('click', mouseClick);
 
-        function MOver(ev) {
-            ev.currentTarget.classList.toggle('highlight');
+        function mouseOver(ev) {
+            event.currentTarget.classList.toggle('highlight');
         }
 
-        function MOut(ev) {
-            ev.currentTarget.classList.toggle('highlight');
+        function mouseOut(ev) {
+            event.currentTarget.classList.toggle('highlight');
         }
+
+        function mouseClick(ev) {
+            event.currentTarget.style.borderColor = "#FFFF00";
+            event.currentTarget.style.backgroundColor = "#FF00FF";
+        }
+
     }
-};   
+
+};
+
